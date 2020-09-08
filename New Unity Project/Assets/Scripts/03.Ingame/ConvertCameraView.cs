4c33983a9f2618291e8a95d9f3ce37113e03443b
TaeYoon <ghddj96@gmail.com>
2020-09-08 14:08:01 +0900

    Add Script : Convcet rCameraView
    시점 변환 코드 추가

---
+++ New Unity Project/Assets/Scripts/03.Ingame/ConvertCameraView.cs
@@ -0,0 +1,42 @@
+﻿using Invector;
+using Invector.vCamera;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ConvertCameraView : MonoBehaviour
+{
+    public vThirdPersonCamera vCam;
+    public vThirdPersonCameraState vCamStates;
+
+    public float FDDistance;
+    public float FDHeight;
+    public float FADistance;
+    public float FAHeight;
+
+    Text currentModeText;
+
+    private void Start()
+    {
+        vCamStates = vCam.CameraStateList.tpCameraStates.Find(delegate (vThirdPersonCameraState obj) { return obj.Name.Equals("Default");});
+        currentModeText = GetComponentInChildren<Text>();
+    }
+    public void ConvertCameraMode()
+    {
+        if(vCamStates.cameraMode == TPCameraMode.FixedAngle)
+        {
+            vCamStates.cameraMode = TPCameraMode.FreeDirectional;
+            vCamStates.defaultDistance = FDDistance;
+            vCamStates.height = FDHeight;
+            currentModeText.text = vCamStates.cameraMode.ToString();
+        }
+        else if(vCamStates.cameraMode == TPCameraMode.FreeDirectional)
+        {
+            vCamStates.cameraMode = TPCameraMode.FixedAngle;
+            vCamStates.defaultDistance = FADistance;
+            vCamStates.height = FAHeight;
+            currentModeText.text = vCamStates.cameraMode.ToString();
+        }
+    }
+}

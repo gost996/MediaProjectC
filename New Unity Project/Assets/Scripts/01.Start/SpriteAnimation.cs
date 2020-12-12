41b1fec6ec43fed72b16f16bafe01430e0b2ddb7
TaeYoon <ghddj96@gmail.com>
2020-12-12 19:27:10 +0900

    Modify Title Backgroud Image
    - 타이틀 배경 이미지 변경
    - 타이틀 배경 이미지 애니메이션 추가

--- New Unity Project/Assets/Scripts/01.Start/SpriteAnimation.cs
+++ New Unity Project/Assets/Scripts/01.Start/SpriteAnimation.cs
@@ -12,7 +12,8 @@ public class SpriteAnimation : MonoBehaviour
         public int spriteNum;
         public float animTime;
         public Image target;
-        public Sprite[] sprites;
+        public bool isRoot;
+        [HideInInspector]public Sprite[] sprites;
     }
 
     public SpriteForAnimation[] spriteList;
@@ -21,35 +22,58 @@ public class SpriteAnimation : MonoBehaviour
 
     UIFadeSystem fadeSystem;
     string path;
+    bool[] triggerLateStart;
 
     // Start is called before the first frame update
     void Awake()
     {
         fadeSystem = GetComponent<UIFadeSystem>();
+        triggerLateStart = new bool[spriteList.Length];
 
-        foreach(SpriteForAnimation sp in spriteList)
+        for(int i = 0; i < spriteList.Length; i++)
         {
+            SpriteForAnimation sp = spriteList[i];
             sp.sprites = new Sprite[sp.spriteNum];
 
             path = sp.spriteName + "/" + sp.spriteName + "_";
-            
-            for (int i = 0; i < sp.spriteNum; i++)
+
+            for (int j = 0; j < sp.spriteNum; j++)
             {
-                Texture2D tx = Resources.Load(path + i) as Texture2D;
-                sp.sprites[i] = Sprite.Create(tx, new Rect(0f, 0f, tx.width, tx.height), new Vector2(0f, 0f));
+                Texture2D tx = Resources.Load(path + j) as Texture2D;
+                sp.sprites[j] = Sprite.Create(tx, new Rect(0f, 0f, tx.width, tx.height), new Vector2(0f, 0f));
             }
 
-            StartCoroutine(Animate(sp.sprites, sp.spriteNum, sp.animTime, sp.target)); 
+            StartCoroutine(Animate(sp.sprites, sp.spriteNum, sp.animTime, sp.target, sp.isRoot, i));
+            StartCoroutine(LateStartUI());
         }
     }
 
-    IEnumerator Animate(Sprite[] _spriteList, int _spriteNum, float _animTime, Image _target)
+    IEnumerator Animate(Sprite[] _spriteList, int _spriteNum, float _animTime, Image _target, bool _isRoot, int _index)
     {
-        for(int i = 0; i < _spriteNum; i++)
+        do
         {
-            _target.sprite = _spriteList[i];
-            yield return new WaitForSeconds(_animTime / _spriteNum);
+            for (int i = 0; i < _spriteNum; i++)
+            {
+                _target.sprite = _spriteList[i];
+                yield return new WaitForSeconds(_animTime / _spriteNum);
+            }
+            triggerLateStart[_index] = true;
         }
+        while (_isRoot);
+
+        yield return null;
+    }
+
+    IEnumerator LateStartUI()
+    {
+        for(int i = 0; i < triggerLateStart.Length; i++)
+        {
+            while (!triggerLateStart[i])
+            {
+                yield return new WaitForEndOfFrame();
+            }
+        }
+
         FadeButton.gameObject.SetActive(true);
         touchTheScreen.gameObject.SetActive(true);
         fadeSystem.StartFade(0);
